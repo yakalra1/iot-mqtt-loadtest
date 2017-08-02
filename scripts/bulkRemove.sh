f2ea4cd8a8bd618c3afdd3328eb2a727cc2d46f6
Yamini Kalra <yakalra1.in.ibm.com>
2017-08-02 12:35:31 +0530

    Code added to remove the auto registered devices for lce_dtu

--- scripts/bulkRemove.sh
+++ scripts/bulkRemove.sh
@@ -42,3 +42,27 @@ echo "Removing devices"
 #eval "curl -s --user '$2':'$3' 'https://$1.internetofthings.ibmcloud.com/api/v0002/bulk/devices/remove' -H 'Content-Type: application/json' -H 'Accept: */*' -H 'Connection: keep-alive' --data-binary '$DEVICE_JSON' --compressed" >> $DEVICE_DEREG_LOG
 eval "curl -s --user '$2':'$3' 'https://$1.internetofthings.chinabluemix.net/api/v0002/bulk/devices/remove' -H 'Content-Type: application/json' -H 'Accept: */*' -H 'Connection: keep-alive' --data-binary '$DEVICE_JSON' --compressed" >> $DEVICE_DEREG_LOG
 #read response and retry for deregistrations that failed.
+
+DTU_DEVICE_JSON="["
+
+last=$(<$DEVICE_FILE wc -l)
+counter=1
+while IFS='=' read -r id pwddevtype; do
+
+    IFS=',' read -r -a pwddevtype_arr <<< "$pwddevtype"
+    deviceType=${pwddevtype_arr[1]}
+
+    if [[ $counter -eq $last ]]; then
+        DTU_DEVICE_JSON="$DTU_DEVICE_JSON{\"typeId\": \"lce_dtu\",\"deviceId\": \"$id\"}]"
+    else
+        DTU_DEVICE_JSON="$DTU_DEVICE_JSON{\"typeId\": \"lce_dtu\",\"deviceId\": \"$id\"},"
+    fi
+    counter=$counter+1
+done <$DEVICE_FILE
+
+#Remove devices
+echo "Removing devices"
+#echo "curl -s --user '$2':'$3' 'https://$1.internetofthings.ibmcloud.com/api/v0002/bulk/devices/remove' -H 'Content-Type: application/json' -H 'Accept: */*' -H 'Connection: keep-alive' --data-binary '$DTU_DEVICE_JSON' --compressed"
+#eval "curl -s --user '$2':'$3' 'https://$1.internetofthings.ibmcloud.com/api/v0002/bulk/devices/remove' -H 'Content-Type: application/json' -H 'Accept: */*' -H 'Connection: keep-alive' --data-binary '$DTU_DEVICE_JSON' --compressed" >> $DEVICE_DEREG_LOG
+eval "curl -s --user '$2':'$3' 'https://$1.internetofthings.chinabluemix.net/api/v0002/bulk/devices/remove' -H 'Content-Type: application/json' -H 'Accept: */*' -H 'Connection: keep-alive' --data-binary '$DTU_DEVICE_JSON' --compressed" >> $DEVICE_DEREG_LOG
+
