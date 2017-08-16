56e14e4fccaa11c17cb244cd725f5dbf813c9069
Yamini Kalra <yakalra1.in.ibm.com>
2017-08-16 23:48:51 +0530

    Changes for Real Time Status Messages

--- scripts/devicePublisher.sh
+++ scripts/devicePublisher.sh
@@ -4,7 +4,8 @@ ORGID=${VCAP_SERVICES_IOTF_SERVICE_0_CREDENTIALS_ORG}
 DEVICEID=$1
 DEVICETYPE=$2
 EVENTTYPE=$3
-topic="iot-2/type/lce_dtu/id/$DEVICEID/evt/$EVENTTYPE/fmt/json"
+#topic="iot-2/type/lce_dtu/id/$DEVICEID/evt/$EVENTTYPE/fmt/json"
+topic="iot-2/type/$DEVICETYPE/id/$DEVICEID/evt/$EVENTTYPE/fmt/json"
 
 #payload template file name <DeviceType>_<Eventtype>.payload 
 #for e.g Demo_status.payload where Demo is DeviceType and status is Eventtype
@@ -28,7 +29,7 @@ while [[ $i -le $NUM_PUB ]]; do
 	payload=$(eval "cat <<EOF
 	$(<$PAYLOAD_NAME)
 	" 2> /dev/null)
-	payload=${payload/EQNUM/$DEVICEID}
+	#payload=${payload/EQNUM/$DEVICEID}
 	#echo "$1 publishing to topic $topic at $NOW_DT"
 	#echo "mosquitto_pub -t $topic -m '$payload' -i $clientID && sleep $SLEEP_PUB"
 	eval "mosquitto_pub -t $topic -m '$payload' -i $clientID && sleep $SLEEP_PUB"
